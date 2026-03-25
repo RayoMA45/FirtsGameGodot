edfa390bea078450c3459ee59289c69e1c50c517
MarcoRayo <rayobm5@gmail.com>
2026-03-24 23:34:48 -0600

    [ADD] interaccion de pesca y animacion

--- Scripts/player.gd
+++ Scripts/player.gd
@@ -1,12 +1,32 @@
 extends CharacterBody2D
 
-func _physics_process(delta):
-	player_movement(delta)
-	
+@onready var tile_marker = $AnimatedSprite2D/Marker2D
+@onready var anim = $AnimatedSprite2D
+
+var startFishing = false
+var waitForFish = false
 const speed = 100
 var current_dir = "none"
 
+func _physics_process(delta):
+	if startFishing: _fishing_state()
+	else: player_movement(delta)
+	
+func _fishing_state(): 
+	velocity = Vector2.ZERO
+	if Input.is_action_just_pressed("ui_cancel"):
+		_stop_fishing()
+	if waitForFish: print("waiting for fish")
+
+func _stop_fishing():
+	waitForFish = false
+	anim.play("Fish_End")
+	move_and_slide()
+
 func player_movement(delta):
+	if Input.is_action_just_pressed("ui_accept"):
+		_start_fishing()
+		return
 	if Input.is_action_pressed("ui_right"):
 		current_dir = "right"
 		play_anim(1)
@@ -31,12 +51,22 @@ func player_movement(delta):
 		play_anim(0)
 		velocity.x = 0
 		velocity.y = 0
-	
 	move_and_slide()
 
+func _start_fishing():
+	if not _get_tile_data() == "water": return
+	startFishing = true
+	waitForFish = false
+	anim.play("Fish_Start")
+
+func _get_tile_data():
+	var tileMap = get_parent().find_child("TileMapLayer")
+	var searchPosition = tileMap.local_to_map(tile_marker.global_position)
+	var data = tileMap.get_cell_tile_data(searchPosition)
+	if data: return data.get_custom_data("type")
+
 func play_anim(movement):
-	var dir = current_dir
-	var anim = $AnimatedSprite2D
+	var dir = current_dir	
 	if dir == "right":
 		anim.flip_h = false
 		if movement == 1:
@@ -61,3 +91,10 @@ func play_anim(movement):
 			anim.play("Back_Walk")
 		elif movement == 0:
 			anim.play("Back_Idle")
+
+func _on_animated_sprite_2d_animation_finished() -> void:
+	if anim.animation == "Fish_Start":
+		waitForFish = true
+		anim.play("Fish_Wait")
+	elif anim.animation == "Fish_End":
+		startFishing = false
